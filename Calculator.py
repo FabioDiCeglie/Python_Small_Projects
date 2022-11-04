d64026c46e05178086ff4d6ad545d33e53ecfa5d
FabioDiCeglie <dcfabio96@gmail.com>
2022-11-04 16:34:35 +0100

    start building the user menu

---
+++ Calculator.py
@@ -0,0 +1,6 @@
+import math
+
+while True:
+    print("\n Choose the math operation. \n\n0 - Addition\n1 - Subtraction\n2 - Multiplication\n3 - Division\n4 - Modulo\n5 - Raising to a power\n6 -Square root\n7 - Logarithm\n9 - Sine\n10 - Cosine\n11 - Tangent\n")
+
+    oper = input("\nYour option from the menu: ")
