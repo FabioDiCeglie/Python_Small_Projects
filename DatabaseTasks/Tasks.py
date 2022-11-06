7782181831b7e8b7c14e7921af9b2d9e5f5803b9
FabioDiCeglie <dcfabio96@gmail.com>
2022-11-06 18:55:09 +0100

    insert the first data in the database

--- DatabaseTasks/Tasks.py
+++ DatabaseTasks/Tasks.py
@@ -23,6 +23,13 @@ cursor.execute('''create table mystaff.employees
        address varchar(50),
        salary int);''')
 
+cursor.execute("insert into mystaff.employees (id,first_name,last_name,department,phone,address,salary) \
+ values (1, 'John', 'Smith', 'Sales', '0123456789', '1st Street, Miami', 50000), \
+        (2, 'Jack', 'Doe', 'IT', '0213456742', '2nd Street, NY', 55000), \
+        (3, 'Emily', 'Davids', 'Sales', '0123456999', '3rd Street, LA', 59000), \
+        (4, 'Karen', 'Willson', 'Logistics', '0823556785', '4th Street, Las Vegas', 41000), \
+        (5, 'Emma', 'Richard', 'Marketing', '0423453580', '5th Street, Denver', 40000);")
+
 
 connection.commit()
 
