5cd784e462b4e6c0fb8ec5225291c7ae1331e6e3
FabioDiCeglie <dcfabio96@gmail.com>
2022-11-06 17:58:59 +0100

    adjust code and wrap up

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,7 @@
 import math
 
 while True:
-    print("\n Choose the math operation. \n\n0 - Addition\n1 - Subtraction\n2 - Multiplication\n3 - Division\n4 - Modulo\n5 - Raising to a power\n6 - Square root\n7 - Logarithm\n9 - Sine\n10 - Cosine\n11 - Tangent\n")
+    print("\n Choose the math operation. \n\n0 - Addition\n1 - Subtraction\n2 - Multiplication\n3 - Division\n4 - Modulo\n5 - Raising to a power\n6 - Square root\n7 - Logarithm\n8 - Sine\n9 - Cosine\n10 - Tangent\n")
 
     oper = input("\nYour option from the menu: ")
 
