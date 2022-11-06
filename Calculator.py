ed64b3a243b55cae1defc2c2f14c1daa86aa02e4
FabioDiCeglie <dcfabio96@gmail.com>
2022-11-06 17:47:58 +0100

    creating method modulo,raising and square root

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,7 @@
 import math
 
 while True:
-    print("\n Choose the math operation. \n\n0 - Addition\n1 - Subtraction\n2 - Multiplication\n3 - Division\n4 - Modulo\n5 - Raising to a power\n6 -Square root\n7 - Logarithm\n9 - Sine\n10 - Cosine\n11 - Tangent\n")
+    print("\n Choose the math operation. \n\n0 - Addition\n1 - Subtraction\n2 - Multiplication\n3 - Division\n4 - Modulo\n5 - Raising to a power\n6 - Square root\n7 - Logarithm\n9 - Sine\n10 - Cosine\n11 - Tangent\n")
 
     oper = input("\nYour option from the menu: ")
 
@@ -54,6 +54,44 @@ while True:
             val2 = float(input("\nSecond value: "))
 
         print("\nThe result is: " + str(val1 / val2) + "\n")
+        back = input("\nGo back to the main menu? (y/n)")
+
+        if back == 'y':
+            continue
+        else:
+            break
+
+    #Modulo
+    elif oper == "4":
+        val1 = float(input("\nFirst value: "))
+        val2 = float(input("\nSecond value: "))
+
+        print("\nThe result is: " + str(val1 % val2) + "\n")
+        back = input("\nGo back to the main menu? (y/n)")
+
+        if back == 'y':
+            continue
+        else:
+            break
+
+    #Raising to a power
+    elif oper == "5":
+        val1 = float(input("\nEnter the base: "))
+        val2 = float(input("\nEnter the power: "))
+
+        print("\nThe result is: " + str(math.pow(val1,val2)) + "\n")
+        back = input("\nGo back to the main menu? (y/n)")
+
+        if back == 'y':
+            continue
+        else:
+            break
+
+    #Square root
+    elif oper == "6":
+        val1 = float(input("\nEnter value for extracting the square root: "))
+
+        print("\nThe result is: " + str(math.sqrt(val1)) + "\n")
         back = input("\nGo back to the main menu? (y/n)")
 
         if back == 'y':
