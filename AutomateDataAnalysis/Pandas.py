25be88008132f06f7adced911e62995770fa70c9
FabioDiCeglie <dcfabio96@gmail.com>
2022-11-07 23:16:48 +0100

    adding second argument in dataFrame

--- AutomateDataAnalysis/Pandas.py
+++ AutomateDataAnalysis/Pandas.py
@@ -1,3 +1,10 @@
 import pandas
 
-d = pandas.DataFrame([['Andy',46,'Engineer'], ['Jane',33,'Nurse'], ['Rober',21,'Student'], ['Maria',30,'Student']])
+# d = pandas.DataFrame([['Andy',46,'Engineer'], ['Jane',33,'Nurse'], ['Rober',21,'Student'], ['Maria',30,'Student']])
+
+# print(type(d))
+# print(d)
+
+
+d = pandas.DataFrame([['Andy',46,'Engineer'], ['Jane',33,'Nurse'], ['Rober',21,'Student'], ['Maria',30,'Student']],columns = ["Name", "Age", "Occupation"])
+print(d)
