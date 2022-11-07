bb360ef06db9c64b9ae296e926be03917187d52f
FabioDiCeglie <dcfabio96@gmail.com>
2022-11-07 23:17:48 +0100

    adding a index

--- AutomateDataAnalysis/Pandas.py
+++ AutomateDataAnalysis/Pandas.py
@@ -6,5 +6,8 @@ import pandas
 # print(d)
 
 
-d = pandas.DataFrame([['Andy',46,'Engineer'], ['Jane',33,'Nurse'], ['Rober',21,'Student'], ['Maria',30,'Student']],columns = ["Name", "Age", "Occupation"])
+# d = pandas.DataFrame([['Andy',46,'Engineer'], ['Jane',33,'Nurse'], ['Rober',21,'Student'], ['Maria',30,'Student']],columns = ["Name", "Age", "Occupation"])
+# print(d)
+
+d = pandas.DataFrame([['Andy',46,'Engineer'], ['Jane',33,'Nurse'], ['Rober',21,'Student'], ['Maria',30,'Student']],columns = ["Name", "Age", "Occupation"], index = ['ID', 'ID2', "ID3", "ID4"])
 print(d)
