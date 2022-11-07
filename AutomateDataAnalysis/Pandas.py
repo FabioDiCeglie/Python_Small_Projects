bb103e29c30a715abc8f25c732ce38e597ed3a0f
FabioDiCeglie <dcfabio96@gmail.com>
2022-11-07 23:26:03 +0100

    create some dummy data

--- AutomateDataAnalysis/Pandas.py
+++ AutomateDataAnalysis/Pandas.py
@@ -9,5 +9,10 @@ import pandas
 # d = pandas.DataFrame([['Andy',46,'Engineer'], ['Jane',33,'Nurse'], ['Rober',21,'Student'], ['Maria',30,'Student']],columns = ["Name", "Age", "Occupation"])
 # print(d)
 
-d = pandas.DataFrame([['Andy',46,'Engineer'], ['Jane',33,'Nurse'], ['Rober',21,'Student'], ['Maria',30,'Student']],columns = ["Name", "Age", "Occupation"], index = ['ID', 'ID2', "ID3", "ID4"])
-print(d)
+# d = pandas.DataFrame([['Andy',46,'Engineer'], ['Jane',33,'Nurse'], ['Rober',21,'Student'], ['Maria',30,'Student']],columns = ["Name", "Age", "Occupation"], index = ['ID', 'ID2', "ID3", "ID4"])
+# print(d)
+# print(d.Name)
+# print(d.Age.min())
+
+dtxt = pandas.read_csv("Employees.txt")
+print(dtxt)
