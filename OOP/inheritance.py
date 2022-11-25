fe6bf6f8fa4651a96af0d7ee1bcd0d518bbbc284
FabioDiCeglie <dcfabio96@gmail.com>
2022-11-25 13:49:56 +0100

    inheritance

---
+++ OOP/inheritance.py
@@ -0,0 +1,24 @@
+class Animal:
+    def __init__(self,bread,color,weight):
+        self.bread = bread
+        self.color = color
+        self.weight = weight
+
+    def walk(self):
+        print("I am walking")
+
+    def eat(self):
+        print("I am eating")
+
+    def sleep(self):
+        print("I am sleeping")
+
+class Dog(Animal):
+    def bark(self):
+        print("I am barking")
+
+
+class Cat(Animal):
+    def meow(self):
+        print("I am meowing")
+
