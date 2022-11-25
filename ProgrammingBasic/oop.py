4166915f4b2e5145a0d30864ac1d1326a4746cee
FabioDiCeglie <dcfabio96@gmail.com>
2022-11-25 10:00:13 +0100

    OOP vs Functional programming

---
+++ ProgrammingBasic/oop.py
@@ -0,0 +1,11 @@
+# Fuctional programming
+def computer_square_area(side_length):
+    return pow(side_length, 2)
+
+# OOP
+class Square:
+    def __init__(self, side_length):
+        self.side_length = side_length
+
+    def compute_area(self):
+        return pow(self.side_length, 2)
