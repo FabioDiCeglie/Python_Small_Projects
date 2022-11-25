5308de99c31fd39558aa5dd7eaf1339555f588fa
FabioDiCeglie <dcfabio96@gmail.com>
2022-11-25 10:05:53 +0100

    Class vs Object

--- ProgrammingBasic/oop.py
+++ ProgrammingBasic/oop.py
@@ -1,3 +1,7 @@
+# what is difference from a class or an object?
+
+   # A class is a blueprint or a module from which we can create instances or object.
+
 # Fuctional programming
 def computer_square_area(side_length):
     return pow(side_length, 2)
@@ -7,5 +11,15 @@ class Square:
     def __init__(self, side_length):
         self.side_length = side_length
 
+# if it is defined inside a class this is called methods
     def compute_area(self):
         return pow(self.side_length, 2)
+
+example_square = Square(15)
+print(example_square.compute_area())
+
+small_square = Square(2)
+print(small_square.compute_area())
+
+big_square = Square(1000)
+print(big_square.compute_area())
