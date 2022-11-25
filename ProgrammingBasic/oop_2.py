4fac076abb000073f54e66d2378a1d31a17e07c5
FabioDiCeglie <dcfabio96@gmail.com>
2022-11-25 13:15:41 +0100

    class vs static methods

--- ProgrammingBasic/oop_2.py
+++ ProgrammingBasic/oop_2.py
@@ -6,6 +6,7 @@ class Email:
         # self.to = to
         self.subject = subject
         self.body = body
+        self.is_from_recipient_valid(self.valid_domain, from_recipient)
 
         split = from_recipient.split("@")
         if split[1] != self.valid_domain:
@@ -20,33 +21,49 @@ class Email:
     #Modify only from the setter
     @from_recipient.setter
     def from_recipient(self, from_recipient):
-        split = from_recipient.split("@")
-        if split[1] != self.valid_domain:
-            raise Exception("Invalid domain")
+        self.is_from_recipient_valid(self.valid_domain, from_recipient)
         self.__from_recipient = from_recipient
 
     def send(self, to):
     #    print(f"Sending email with subject: {self.subject} and body: {self.body} to: {self.to}")
-     print(f"Sending email from: {self.__from_recipient} with subject: {self.subject} and body: {self.body} to: {to}")
+         print(f"Sending email from: {self.__from_recipient} with subject: {self.subject} and body: {self.body} to: {to}")
+
+    @classmethod
+    def greeting(cls):
+        return cls("Welcome!", "Welcome to epicpython.io!", "greeting@epicpython.io")
+
+    @staticmethod
+    def is_from_recipient_valid(valid_domain, from_recipient):
+        splitted = from_recipient.split("@")
+        if splitted[1] != valid_domain:
+            raise Exception("Invalid domain")
 
 
 
-greeting_email = Email("Welcome", "Welcome to epicpython.io", "greeting@epicpython.io")
-bf_email = Email("Black Friday 2022", "50% discount", "bf@epicpython.io")
 
-Email.valid_domain = "example.com"
+# greeting_email = Email.greeting()
+# bf_email = Email("Black Friday 2022", "50% discount", "bf@epicpython.io")
+
+# Email.valid_domain = "example.com"
 # greeting_email = Email("Welcome", "Welcome to epicpython.io", "greeting@hello.io") #Error
 # greeting_email._from_recipient = "welcome@ciao.com" # here we are not restricted
 # greeting_email.__from_recipient = "welcome@ciao.com" # here we are restricted because of the __ underscores because it become private but with @property we are not restricted but it will not work
 
-greeting_email.from_recipient = "greeting@example.com"
-bf_email.from_recipient = "bf@example.com"
-greeting_email.subject = "Greeting"
+# greeting_email.from_recipient = "greeting@example.com"
+# bf_email.from_recipient = "bf@example.com"
+# greeting_email.subject = "Greeting"
 
 # print(greeting_email.send())
-
 # print(greeting_email.send("ciao.com"))
 
-print(bf_email.send("alex@gmail.com"))
+# print(bf_email.send("alex@gmail.com"))
+greeting_email = Email.greeting()
+bf_email = Email("Black Friday 2020!", "50% discounts only on our platform, don't miss them!", "bf@epicpython.io")
 
+bf_email.from_recipient = "bf@epicpython.uk"
+
+greeting_email.send("bob@nasa.gov")
+bf_email.send("alex@gmail.com")
+
+#it will give an error because the domain is not correct
 
